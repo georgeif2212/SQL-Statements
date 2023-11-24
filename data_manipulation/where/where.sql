682a57757b90fbf609d62fb691b4b4266bd18d9c
georgeif2212 <jinfante2212@gmail.com>
2023-11-24 16:37:31 -0600

    where statement with IN operator

--- data_manipulation/where/where.sql
+++ data_manipulation/where/where.sql
@@ -19,3 +19,11 @@ SELECT * FROM TRACK WHERE Milliseconds > 300000;
 -- ? Muestra todos los clientes de la tabla customers cuyo país comienza con la letra "C".
 
 SELECT * FROM Customer WHERE Country LIKE 'C%';
+
+-- ? Filtrar con IN:
+
+-- ? Muestra todas las canciones de la tabla tracks que pertenezcan a los géneros "Rock" o "Metal".
+
+-- ! Los id's 1 y 3 corresponden al genero Rock y metal
+
+SELECT * FROM Track WHERE GenreId IN (1,3);
