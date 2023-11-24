31357db5b894231e51d5d073a207b15d06fb78b2
georgeif2212 <jinfante2212@gmail.com>
2023-11-24 16:23:30 -0600

    where statement that satisfies the condition city ='Lethbridge'

---
+++ data_manipulation/where/where.sql
@@ -0,0 +1,9 @@
+﻿-- * En estos ejercicios estaré trabajando con la base de datos Chinook
+
+use Chinook;
+
+-- ? Filtrar por Valor Específico:
+
+-- ? Muestra todos los registros de la tabla Employee donde la cidad sea "Lethbridge".
+
+SELECT * FROM dbo.Employee WHERE City = 'Lethbridge';
