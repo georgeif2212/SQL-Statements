f9e0dc7b28b669a3cc332b071f0bc2a12435c063
georgeif2212 <jinfante2212@gmail.com>
2023-11-25 00:06:04 -0600

    An statement to customers who are from USA and their name starts with J

--- data_manipulation/or/or.sql
+++ data_manipulation/or/or.sql
@@ -1,8 +1,15 @@
 ﻿-- * En estos ejercicios estaré trabajando con la base de datos Chinook
--- ? Filtrar por Género:
--- ? Muestra todas las canciones de la tabla tracks que son de género 'Rock' o 'Pop'.
 
 USE CHINOOK;
 
+-- ? Filtrar por Género:
+
+-- ? Muestra todas las canciones de la tabla tracks que son de género 'Rock' o 'Pop'.
 
 SELECT * FROM Track WHERE GenreId = 1 OR GenreId = 9;
+
+-- ? Filtrar por Cliente o País:
+
+-- ? Muestra todos los clientes de la tabla customers que sean de 'USA' o su nombre comience con "J".
+
+SELECT * FROM Customer WHERE Country = 'USA' OR FirstName LIKE 'J%';
