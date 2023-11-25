984552a46f22e44eeeafce18803ba680e39dabb3
georgeif2212 <jinfante2212@gmail.com>
2023-11-25 00:01:23 -0600

    SQL Script showing the use of OR operator

---
+++ data_manipulation/or/or.sql
@@ -0,0 +1,8 @@
+﻿-- * En estos ejercicios estaré trabajando con la base de datos Chinook
+-- ? Filtrar por Género:
+-- ? Muestra todas las canciones de la tabla tracks que son de género 'Rock' o 'Pop'.
+
+USE CHINOOK;
+
+
+SELECT * FROM Track WHERE GenreId = 1 OR GenreId = 9;
