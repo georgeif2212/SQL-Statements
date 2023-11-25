173099346ebcff347bcb72a536e47f377d8bddad
georgeif2212 <jinfante2212@gmail.com>
2023-11-24 19:42:28 -0600

    SQL Script which uses AdventureWorksDW database with AND operator

---
+++ data_manipulation/and/and.sql
@@ -0,0 +1,8 @@
+﻿-- * En estos ejercicios estaré trabajando con la base de datos Chinook
+use AdventureWorksDW2022
+
+-- ? Filtrar por Rango de Fechas:
+
+-- ? Muestra todas las ventas de la tabla FactInternetSales donde la fecha de venta (OrderDate) está entre '2013-01-01' y '2013-01-03'.
+
+SELECT * FROM FactInternetSales WHERE OrderDate BETWEEN '2013-01-01' AND '2013-01-03';
