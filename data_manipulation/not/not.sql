d954405811a3f10d3145bd35035f84418ef88966
georgeif2212 <jinfante2212@gmail.com>
2023-11-26 14:04:23 -0600

    SQL Script that shows use of NOT operator

---
+++ data_manipulation/not/not.sql
@@ -0,0 +1,13 @@
+﻿USE Northwind;
+
+-- ? Filtrar por Productos que NO sean de la Categoría 'Beverages':
+
+-- ? Muestra todos los productos de la tabla Products que NO pertenezcan a la categoría 'Beverages', ni 'Seafood'
+
+-- ? La categoría 'Beverages' tiene el id 1 y 'Seafood tiene el id 8'
+
+SELECT *
+FROM Products
+WHERE
+    NOT CategoryID = 1
+    AND NOT CategoryID = 8;
