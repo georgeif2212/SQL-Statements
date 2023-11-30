795c31af236ace90d7c4b953f70dd0e277fa88d6
georgeif2212 <jinfante2212@gmail.com>
2023-11-30 14:26:54 -0600

    SQL Script with one example which shows an exercise

---
+++ data_manipulation/insert-into/insertInto.sql
@@ -0,0 +1,27 @@
+﻿-- * Para estos ejercicios estaré utilizando la base de datos Chinook
+
+use Chinook;
+
+SELECT * FROM Customer;
+
+-- ? Inserta un nuevo cliente llamado Jorge Infante en la tabla Customer en la base de datos "Chinook".
+
+-- ? Proporciónale Company, Country y Email. Asegúrate de que el CustomerID sea único.
+
+INSERT INTO
+    Customer (
+        CustomerId,
+        FirstName,
+        LastName,
+        Company,
+        Country,
+        Email
+    )
+VALUES (
+        60,
+        'Jorge',
+        'Infante',
+        'UAMC',
+        'México',
+        'jinfante2212@gmail.com'
+    );
