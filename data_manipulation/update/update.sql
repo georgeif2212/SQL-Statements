19b77dddee55b3ad1c56d20b1d8513c72faea62e
georgeif2212 <jinfante2212@gmail.com>
2023-11-30 21:43:02 -0600

    SQL Script with one exercise about UPDATE Statement

---
+++ data_manipulation/update/update.sql
@@ -0,0 +1,11 @@
+﻿-- * Para estos ejercicios estaré utilizando la base de datos AdventureWorksDW
+
+USE AdventureWorksDW2022;
+
+-- Active: 1697852858102@@127.0.0.1@3306
+
+-- ? Actualizar el Precio de todos los Productos:
+
+-- ? Incrementa en un 10% el precio (ListPrice) de todos los productos en la tabla DimProduct.
+
+UPDATE DimProduct SET ListPrice = ListPrice * 1.1;
