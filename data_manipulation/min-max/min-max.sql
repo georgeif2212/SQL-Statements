32dcc8177d1aacfde65a961a3c291f9e088bda11
georgeif2212 <jinfante2212@gmail.com>
2023-12-01 13:20:59 -0600

    SQL Script with examples of MIN and MAX SQL functions

---
+++ data_manipulation/min-max/min-max.sql
@@ -0,0 +1,13 @@
+﻿-- * Para estos ejercicios estaré utilizando la base de datos AdventureWorksDW
+
+USE AdventureWorksDW2022;
+
+-- ? Obtener el Precio Mínimo de Productos:
+
+-- ? Obtener el precio más bajo (ListPrice) de la tabla DimProduct
+
+-- ? que pertenezca a la subcategoría con id 2.
+
+SELECT MIN(ListPrice)
+FROM DimProduct
+WHERE ProductSubcategoryKey = 2;
