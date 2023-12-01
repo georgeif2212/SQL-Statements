7fd9b4f30cd5c572f8ac0cd5ad88df18c7dce445
georgeif2212 <jinfante2212@gmail.com>
2023-12-01 13:52:17 -0600

    SQL Script with COUNT Function examples

---
+++ data_manipulation/count/count.sql
@@ -0,0 +1,9 @@
+﻿-- * Para estos ejercicios estaré utilizando la base de datos AdventureWorksDW
+
+USE AdventureWorksDW2022
+
+-- ? Contar la Cantidad de Empleados:
+
+-- ? Obtener la cantidad total de empleados en la tabla DimEmployee.
+
+SELECT COUNT(*) AS EmployeeNumber FROM DimEmployee;
