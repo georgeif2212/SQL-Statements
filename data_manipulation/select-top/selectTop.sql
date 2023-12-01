2826f7d27921d8ee4be202c377c20ba7dc676001
georgeif2212 <jinfante2212@gmail.com>
2023-12-01 12:55:08 -0600

    Another example using SELECT TOP Clause

--- data_manipulation/select-top/selectTop.sql
+++ data_manipulation/select-top/selectTop.sql
@@ -10,4 +10,8 @@ SELECT TOP 5 * FROM Track ORDER BY Milliseconds DESC;
 
 -- ? Seleccionar los albumes del artista con id 1 ordenados por titulo alfabeticamente:
 
-SELECT * FROM Album WHERE ArtistId=1 ORDER BY Title;
+SELECT * FROM Album WHERE ArtistId = 1 ORDER BY Title;
+
+-- ? Seleccionar las primeras 5 facturas del cliente con ID 2 ordenadas por el total:
+
+SELECT TOP 5 * FROM Invoice WHERE CustomerId = 2 ORDER BY Total;
