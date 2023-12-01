2ff5d205932a0436af32fedaccd455addbf04e93
georgeif2212 <jinfante2212@gmail.com>
2023-11-30 21:50:44 -0600

    Another example but updating specific register

--- data_manipulation/update/update.sql
+++ data_manipulation/update/update.sql
@@ -2,10 +2,18 @@
 
 USE AdventureWorksDW2022;
 
--- Active: 1697852858102@@127.0.0.1@3306
-
 -- ? Actualizar el Precio de todos los Productos:
 
 -- ? Incrementa en un 10% el precio (ListPrice) de todos los productos en la tabla DimProduct.
 
 UPDATE DimProduct SET ListPrice = ListPrice * 1.1;
+
+-- ? Actualizar el Salario Base de un empleado:
+
+-- ? Aumenta el salario base (BaseRate) del empleado con ID 89 de la tabla DimEmployee en un 15%.
+
+UPDATE DimEmployee
+SET BaseRate = BaseRate * 1.15
+WHERE EmployeeKey = 89;
+
+SELECT * FROM DimEmployee WHERE EmployeeKey = 89;
