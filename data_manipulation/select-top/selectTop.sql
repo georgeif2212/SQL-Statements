538bd43afd899157183c647d9efd0ffea49c60f8
georgeif2212 <jinfante2212@gmail.com>
2023-12-01 12:48:48 -0600

    Second exercise with SELECT TOP CLAUSE

--- data_manipulation/select-top/selectTop.sql
+++ data_manipulation/select-top/selectTop.sql
@@ -7,3 +7,7 @@ USE Chinook;
 -- ? Obtener las 5 canciones más largas de la tabla tracks basado en la duración.
 
 SELECT TOP 5 * FROM Track ORDER BY Milliseconds DESC;
+
+-- ? Seleccionar los albumes del artista con id 1 ordenados por titulo alfabeticamente:
+
+SELECT * FROM Album WHERE ArtistId=1 ORDER BY Title;
