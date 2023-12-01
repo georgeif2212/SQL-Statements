c3b67417bcc0566f1bc89b48fda3c754b192353b
georgeif2212 <jinfante2212@gmail.com>
2023-11-30 20:34:44 -0600

    SQL Script showing NULL functions

---
+++ data_manipulation/null-value/null.sql
@@ -0,0 +1,26 @@
+﻿-- * Para estos ejercicios estaré utilizando la base de datos Northwind
+use Northwind;
+
+-- ? Filtrar Registros con Valor NULL:
+
+-- ? Muestra todos los productos de la tabla Products en la base de datos
+
+-- ? "Northwind" que tienen un valor NULL en la columna SupplierID.
+
+SELECT * FROM Products WHERE SupplierID IS NULL;
+
+-- ? Inserta registros con Valor NULL:
+
+-- ? Inserta un registro en donde insertes valores para los campos
+
+-- ? ProductName, SupplierID, Discontinued, mientras que los demás quedan en null
+
+INSERT INTO
+    PRODUCTS (
+        ProductName,
+        SupplierID,
+        Discontinued
+    )
+VALUES ('Gomitas', 3, 0);
+
+SELECT * FROM Products;
