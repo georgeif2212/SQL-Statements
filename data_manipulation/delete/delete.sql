e5590e80d7b7183a9577d15ef0dc1cb84320e19a
georgeif2212 <jinfante2212@gmail.com>
2023-12-01 11:16:05 -0600

    SQL Script with Delete statement unfinished

---
+++ data_manipulation/delete/delete.sql
@@ -0,0 +1,9 @@
+﻿-- * Para estos ejercicios estaré utilizando la base de datos AdventureWorksDW
+
+USE AdventureWorksDW2022;
+
+-- ? Eliminar Empleados despedidos:
+
+-- ? Elimina todos los empleados de la tabla DimEmployee donde la fecha de
+
+-- ? despido (EndDate) no sea NULL.
