01bc38ac0c1ae1902c1812d48303183989307fe6
georgeif2212 <jinfante2212@gmail.com>
2023-12-01 12:34:08 -0600

    First exercise showing SELECT TOP Clause

---
+++ data_manipulation/select-top/selectTop.sql
@@ -0,0 +1,9 @@
+﻿-- * Para estos ejercicios estaré utilizando la base de datos Chinook
+
+USE Chinook;
+
+-- ? Seleccionar las 5 Canciones más Largas:
+
+-- ? Obtener las 5 canciones más largas de la tabla tracks basado en la duración.
+
+SELECT TOP 5 * FROM Track ORDER BY Milliseconds DESC;
