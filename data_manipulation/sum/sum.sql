19eec87ed3d3a3fcfeb9ec71391c06018ab3a54a
georgeif2212 <jinfante2212@gmail.com>
2023-12-11 17:55:53 -0600

    Exercise with arithmetic expression

--- data_manipulation/sum/sum.sql
+++ data_manipulation/sum/sum.sql
@@ -1,7 +1,17 @@
-﻿USE Northwind
+﻿-- * Para estos ejercicios estaré utilizando la base de datos Northwind
+
+USE Northwind
 
 -- ? Sumar la Cantidad Total de Unidades Vendidas:
 
 -- ? Utiliza SUM para obtener la cantidad total de unidades vendidas (Quantity) en la tabla OrderDetails
 
 SELECT SUM(Quantity) AS TotalUnidadesVendidas FROM [Order Details]
+
+-- ? Sumar el Precio Total de Productos en Inventario:
+
+-- ? Utiliza SUM para obtener el precio total de todos los productos en inventario en la tabla Products.
+
+SELECT
+    SUM(UnitPrice * UnitsInStock) AS PrecioTotalProductosEnInventario
+FROM Products;
