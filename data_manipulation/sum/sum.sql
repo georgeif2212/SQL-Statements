3032b808790fb760544eaeea314035794039a23b
georgeif2212 <jinfante2212@gmail.com>
2023-12-11 14:31:53 -0600

    SQL script with exercises that shows the sum function

---
+++ data_manipulation/sum/sum.sql
@@ -0,0 +1,7 @@
+﻿USE Northwind
+
+-- ? Sumar la Cantidad Total de Unidades Vendidas:
+
+-- ? Utiliza SUM para obtener la cantidad total de unidades vendidas (Quantity) en la tabla OrderDetails
+
+SELECT SUM(Quantity) AS TotalUnidadesVendidas FROM [Order Details]
