e23ee9076af37742974a524860bfdeb37b224a46
georgeif2212 <jinfante2212@gmail.com>
2023-12-11 18:30:36 -0600

    SQL script with more average function examples

--- data_manipulation/avg/avg.sql
+++ data_manipulation/avg/avg.sql
@@ -4,6 +4,12 @@ USE Chinook;
 
 -- ? Calcular el Tiempo Promedio de las Canciones:
 
--- ? Utiliza AVG para calcular el tiempo promedio (UnitPrice) de todas las canciones en la tabla tracks.
+-- ? Utiliza AVG para calcular el tiempo promedio (Miliseconds) de todas las canciones en la tabla tracks.
 
 SELECT AVG(Milliseconds) AS TiempoPromedio FROM Track;
+
+-- ? Calcular el Promedio del total de las Facturas:
+
+-- ? Utiliza AVG para calcular el tiempo promedio (Total) de todas las facturas en la tabla Invoice.
+
+SELECT AVG(Total) AS PromedioTotal FROM Invoice;
