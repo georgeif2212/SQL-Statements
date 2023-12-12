d446cae3f44d910890666459da0af35b950bfe05
georgeif2212 <jinfante2212@gmail.com>
2023-12-12 09:18:57 -0600

    SQL script using wildcard characters with Like operator

---
+++ data_manipulation/wildcards_characters/wildcard-characters.sql
@@ -0,0 +1,19 @@
+﻿-- * En estos ejercicios estaré trabajando con la base de datos AdventureWorksDW2022
+
+use AdventureWorksDW2022;
+
+-- ? Buscar Productos por Nombre que Comienzan con "M":
+
+-- ? Utiliza el carácter comodín % para buscar productos cuyo nombre comience con la letra "M" en la tabla DimProduct.
+
+SELECT * FROM DimProduct WHERE EnglishProductName LIKE 'M%'
+
+-- ? Buscar Empleados por Apellido que Comience con "J","H", O
+
+-- ? Utiliza [] para buscar empleados cuyo apellido comience con "JOH"
+
+SELECT * FROM DimEmployee WHERE LastName LIKE '[Joh]%';
+
+-- ? Buscar Productos por Nombre que Contenga "Crank":
+
+SELECT * FROM DimProduct WHERE EnglishProductName LIKE '%Crank%';
