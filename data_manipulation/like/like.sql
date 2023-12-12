66756b383e1b9aa0ea87486cadd7a9ff406d1a76
georgeif2212 <jinfante2212@gmail.com>
2023-12-11 19:09:05 -0600

    SQL Scritpt with examples that shows the uses of Like operator

---
+++ data_manipulation/like/like.sql
@@ -0,0 +1,21 @@
+﻿-- * Para estos ejercicios estaré utilizando la base de datos Northwind
+
+use Northwind;
+
+-- ? Buscar Productos por Nombre:
+
+-- ? Utiliza LIKE para buscar productos cuyo nombre contenga la palabra "to" en la tabla Products.
+
+SELECT * FROM Products WHERE ProductName LIKE '%to%';
+
+-- ? Buscar Empleados por Apellido:
+
+-- ? Utiliza LIKE para buscar empleados cuyo apellido comience con "D" en la tabla Employees.
+
+SELECT * FROM Employees WHERE LastName LIKE 'D%';
+
+-- ? Buscar Proveedores por Nombre:
+
+-- ? Utiliza LIKE para buscar empleados cuyo nombre termine con "d" en la tabla Employees.
+
+SELECT * FROM Suppliers WHERE ContactName LIKE '%d';
