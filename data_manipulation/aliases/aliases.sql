1636c760164a2a75df9bdd8f5d64bacd84f5a87a
georgeif2212 <jinfante2212@gmail.com>
2023-12-13 09:39:58 -0600

    SQL Script with alises exercises

---
+++ data_manipulation/aliases/aliases.sql
@@ -0,0 +1,23 @@
+﻿-- * Para estos ejercicios estaré utilizando la base de datos Northwind
+
+USE Northwind;
+
+-- ? Alias para la columna "ProductName" como "Name", UnitPrice como "Price" en "Products":
+
+SELECT
+    ProductID,
+    ProductName AS Name,
+    UnitPrice AS Price
+FROM Products;
+
+-- ? Alias para la columna "OrderDate" en "Orders":
+
+SELECT OrderID, CustomerID, OrderDate AS Date FROM Orders;
+
+--Alias para la expresión de concatenación de direcciones en "Customers":
+
+SELECT
+    CustomerID,
+    ContactName,
+    Address + ', ' + City + ', ' + Country AS FullAddress
+FROM Customers;
