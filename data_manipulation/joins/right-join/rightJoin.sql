93fb364e5876756975e0abee090517053f8f8e15
georgeif2212 <jinfante2212@gmail.com>
2024-01-13 16:44:23 -0600

    feat: SQL script with exercises showing the use of right join keyword

---
+++ data_manipulation/joins/right-join/rightJoin.sql
@@ -0,0 +1,14 @@
+﻿-- * Para estos ejercicios estaré utilizando la base de datos AdventureWorksDW
+
+use AdventureWorksDW2022;
+
+-- ? Mostrar Detalles de Producto y sus Ventas (si las tienen):
+-- ? Combina la información de la tabla DimProduct con la tabla FactInternetSales utilizando un LEFT JOIN para mostrar todos los detalles de producto y las ventas correspondientes si las tienen.
+
+SELECT
+    DimProduct.ProductKey,
+    DimProduct.EnglishProductName,
+    DimProduct.StandardCost,
+    FactInternetSales.OrderDate
+FROM FactInternetSales
+    RIGHT JOIN DimProduct ON FactInternetSales.ProductKey = DimProduct.ProductKey;
