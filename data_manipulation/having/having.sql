386fbfd33587c6204c407e25087cc001ff32f6c8
georgeif2212 <jinfante2212@gmail.com>
2024-01-23 15:43:22 -0600

    feat: script that shows categories with 5 or more products

---
+++ data_manipulation/having/having.sql
@@ -0,0 +1,11 @@
+﻿-- * Para estos ejercicios estaré utilizando la base de datos Northwind
+
+USE Northwind
+-- ? Categorías con más de 5 productos:
+-- ? Encuentra las categorías que tienen más de 5 productos.
+
+SELECT Categories.CategoryName, COUNT(ProductID) AS ProductsPerCategory
+FROM Products
+    INNER JOIN Categories ON Categories.CategoryID = Products.CategoryID
+GROUP BY
+    Categories.CategoryName;
