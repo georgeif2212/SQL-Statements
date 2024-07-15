c852053a75a67c310904ed6fabbf33fc8637f5f3
georgeif2212 <jinfante2212@gmail.com>
2024-07-15 17:55:00 -0600

    feat: new example using CASE expression}

--- data_manipulation/case/case.sql
+++ data_manipulation/case/case.sql
@@ -3,8 +3,6 @@
 -- ? Clasificación de Duración de Canciones:
 -- ? Clasifica las canciones de la tabla Track en tres categorías: "Corta" (menos de 3 minutos), "Media" (entre 3 y 5 minutos) y "Larga" (más de 5 minutos).
 
---SELECT Milliseconds FROM Track;
-
 SELECT
     TrackId,
     Name,
@@ -15,3 +13,18 @@ SELECT
         ELSE 'Larga'
     END AS Duration
 FROM Track;
+
+-- ? Descuento por Cantidad en Facturas:
+-- ? Crea una consulta que muestre el InvoiceId, CustomerId, y un campo calculado Discount que otorgue un 10% de descuento si el total de la factura es mayor a $50,
+-- ? y un 20% de descuento si es mayor a $100. Para los totales menores o iguales a $50, el descuento es del 0%.
+
+SELECT
+    InvoiceId,
+    CustomerId,
+    Total,
+    CASE
+        WHEN Total <= 3 THEN '0%'
+        WHEN Total > 3 AND Total < 10 THEN '10%'
+        ELSE '20%'
+    END AS Discount
+FROM Invoice;
