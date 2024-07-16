6a18aaa149b35ac98cee91efd623a00fa9327b81
georgeif2212 <jinfante2212@gmail.com>
2024-07-15 18:36:51 -0600

    feat: new example creating Stored Procedures

---
+++ data_manipulation/stored-procedures/storedProcedures.sql
@@ -0,0 +1,11 @@
+﻿USE Chinook;
+
+-- ? Crear un Stored Procedure para Obtener Todas las playlists:
+-- ? Crea un stored procedure que de SELECT a todas las playlist de la tabla Playlist.
+
+CREATE PROCEDURE SelectAllPlaylists
+AS
+SELECT * FROM Playlist
+GO;
+
+SelectAllPlaylists
